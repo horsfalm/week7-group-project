a85b31f81182298b9a42c0230837e01dd3b00739
Jeffrey Jing <jjing@gmail.com>
2021-12-14 21:02:30 -0800

    movies mark 1 functional, either webpage or poster needs size adjustment. user input adjustments needed as well.

---
+++ assets/js/movies.js
@@ -0,0 +1,33 @@
+// Source code by Jeffrey Jing
+
+
+// Makes an API call to the online movie database with a given input, captures
+// the returned values from the API request in JSON format, and displays a 
+// poster of the movie based on input.
+function getMovies(input) {
+    var url = "http://www.omdbapi.com/?apikey=e19afb39&s=";
+    url += input;
+    fetch(url, {
+        "method": "GET",
+        "headers": {}
+    })
+    .then(response => response.json())
+    .then(res => {
+        console.log(res);
+        
+        // previous code, will probably delete soon
+        // do stuff with res here. res is a json variable of all the data
+        //for (var i = 0; i < res.Search.length; i++) {
+        //    var inner = '<div>' + res.Search[i].Title + "</div>";
+        //    document.getElementById('films').innerHTML += inner;
+        //}
+
+        let poster = res.Search[0].Poster; // can change search[0] to search[Math.rand(Search.length)] later
+        var inner = "<div>" + "<img src=\"" + poster + "\">" + "</div>"; //"\" width=\"500\" height=\"200\">" + "</div>"; <- attempted to set a size for poster, but it loses resolution.
+        document.getElementById('films').innerHTML = inner; 
+
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
